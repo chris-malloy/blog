3916ff3ea637f88db08c81b73a916e403ac39a40
chris-malloy <chrismalloymusic@gmail.com>
2018-02-27 17:18:21 -0500

    set up react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as BR, Route } from 'react-router-dom';
+import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import PostsIndex from './components/PostsIndex';
+
+const store = applyMiddleware()(createStore)(rootReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <div className="">
+            <BR>
+                <div>
+                    <Route path="/" component={PostsIndex} />
+                </div>
+            </BR>
+        </div>
+    </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
