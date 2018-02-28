ede4c60b95bdebe32da77750c3c1f2a3aadc6ce0
chris-malloy <chrismalloymusic@gmail.com>
2018-02-28 17:51:11 -0500

    form validation and error handling

--- src/components/PostNew.js
+++ src/components/PostNew.js
@@ -2,22 +2,38 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+	// helper method
+	// give access to field object from redux-form
 	renderField(field) {
+		// destructure touched and error from meta from field
+		const { meta: { touched, error} } = field;
+		const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+		
 		return (
-			<div className="form-group">
+			<div className={className}>
 				<label>{field.label}</label>
 				<input
 					className="form-control"
 					type="text"
 					{...field.input}
 				/>
+				<div className="text-help">
+					{touched ? error : ''}
+				</div>
 			</div>		
 		)
 	}
 
+	onSubmit(values) { 
+		console.log(values);
+	}
+
 	render() {
+		// handleSubmit comes from redux-form
+		const { handleSubmit } = this.props;
+
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 				<Field
 					label="Title"
 					name="title"
@@ -33,11 +49,30 @@ class PostsNew extends Component {
 					name="content"
 					component={this.renderField}
 				/>
+				<button type="submit" className="btn btn-primary">Submit</button>
 			</form>
 		)
 	}
 }
 
+// error handling for redux-form
+function validate(values){
+	const errors = {};
+
+	if (!values.title) {
+		errors.title = "Please enter a title.";
+	}
+	if (!values.categories) {
+		errors.categories = "Please enter some categories";
+	}
+	if (!values.content) {
+		errors.content = "Please enter some content.";
+	}
+
+	return errors;
+}
+
 export default reduxForm({
+	validate,
 	form: 'PostsNewForm'
 })(PostsNew);
