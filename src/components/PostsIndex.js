ee7f343ad45d9a940b37126e57691907a150c68e
chris-malloy <chrismalloymusic@gmail.com>
2018-02-28 12:53:53 -0500

    posts reducer and action, new post component

--- src/components/PostsIndex.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class PostsIndex extends Component {
-    render() {
-        return (
-            <div>Post Index Component</div>
-        )
-    }
-}
--- src/index.js
+++ src/index.js
@@ -3,23 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import rp from 'redux-promise';
 import { BrowserRouter as BR, Route } from 'react-router-dom';
 import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-import PostsIndex from './components/PostsIndex';
+import PostsIndex from './containers/PostsIndex';
+import PostsNew from './components/PostNew';
 
-const store = applyMiddleware()(createStore)(rootReducer);
+const store = applyMiddleware(rp)(createStore)(rootReducer);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <div className="">
-            <BR>
-                <div>
-                    <Route path="/" component={PostsIndex} />
-                </div>
-            </BR>
-        </div>
-    </Provider>
+	<Provider store={store}>
+		<div className="container">
+			<BR>
+				<div>
+						<Route exact path="/" component={PostsIndex} />
+						<Route path="/posts/new" component={PostsNew} />
+				</div>
+			</BR>
+		</div>
+	</Provider>
 , document.getElementById('root'));
 registerServiceWorker();
